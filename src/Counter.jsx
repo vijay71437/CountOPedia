a6d8a0fc41f8b9b230fb564bdbe573530f3c4318
vijay71437 <vijaymcse2024@gmail.com>
2025-11-22 00:25:49 +0530

    learn use state and useeffect in react and how to use objects in useState

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,22 +1,88 @@
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
+import attackImg from "./images/attack.png";
+import defenddkImg from "./images/defend.png";
 function Counter() {
-  let [count, setCount] = useState(0);
+  const [gameState, setGameState] = useState({
+    count: 0,
+    gameStatus: "",
+  });
+
   function handleIncrement() {
-    setCount(count++);
+    const newValue = gameState.count + 1;
+    const status =
+      newValue >= 5 ? "You Won!" : newValue <= -5 ? "You Lost!" : "";
+    setGameState({ count: newValue, gameStatus: status });
   }
   function handleDecrement() {
-    setCount(count--);
+    const newValue = gameState.count - 1;
+    const status =
+      newValue <= -5 ? "You Lost!" : newValue >= 5 ? "You Won!" : "";
+    setGameState({ count: newValue, gameStatus: status });
+  }
+
+  function handleRandomPlay() {
+    const playMode = Math.round(Math.random());
+    if (playMode == 0) {
+      handleIncrement();
+    } else {
+      handleDecrement();
+    }
+  }
+  function handleReset() {
+    setGameState({ count: 0, gameStatus: "" });
+
+    // setGameState((prev) => ({
+    //   ...prev,
+    //   gameStatus: "",
+    // }));
   }
   return (
-    <div className="row text-white container">
-      Counter: {count}
-      <button className="btn btn-success m-2" onClick={handleIncrement}>
-        +1
-      </button>
-      <button className="btn btn-danger m-2" onClick={handleDecrement}>
-        -1
-      </button>
+    <div className="container">
+      <div className="row text-white text-center">
+        <h1>Game Score: {gameState.count} </h1>
+        <p>You win at +5 points and lose at -5 points!</p>
+        {gameState.gameStatus.length > 0 && (
+          <h3>Game Status:{gameState.gameStatus}</h3>
+        )}
+
+        <div className="col-6 col-md-3 offset-md-3">
+          <img
+            src={attackImg}
+            alt=""
+            style={{
+              width: "100%",
+              cursor: "pointer",
+              border: "1px solid green",
+            }}
+            className="p-4 rounded"
+            onClick={handleIncrement}
+          />
+        </div>
+
+        <div className="col-6 col-md-3 offset-md-3">
+          <img
+            src={defenddkImg}
+            alt=""
+            style={{
+              width: "100%",
+              cursor: "pointer",
+              border: "1px solid red",
+            }}
+            className="p-4 rounded"
+            onClick={handleDecrement}
+          />
+        </div>
+        <div className="col-12 col-md-4 offset-md-4"></div>
+        <button
+          className="btn btn-success m-2 w-100"
+          onClick={handleRandomPlay}
+        >
+          Random Play
+        </button>
+        <button className="btn btn-danger m-2 w-100" onClick={handleReset}>
+          Reset
+        </button>
+      </div>
     </div>
   );
 }
